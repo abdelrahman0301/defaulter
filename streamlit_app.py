d0aeb3eaba666ae4fcc1ecad7bb9dfc307a85063
abdelrahman0301 <142699953+abdelrahman0301@users.noreply.github.com>
2025-12-06 11:22:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,40 +27,40 @@ with st.form("prediction_form"):
     # -------------------------
     with col1:
         st.subheader("Personal & ID")
-        sk_id_curr = st.number_input("SK_ID_CURR", value=370868)
-        years_birth = st.number_input("YEARS_BIRTH (Age)", value=33.670622)
-        years_employed = st.number_input("YEARS_EMPLOYED", value=3.375746)
-        years_registration = st.number_input("YEARS_REGISTRATION", value=4.807086)
-        years_id_publish = st.number_input("YEARS_ID_PUBLISH", value=6.615236)
-        cnt_fam_members = st.number_input("CNT_FAM_MEMBERS", value=4.037309)
+        sk_id_curr = st.number_input("SK_ID_CURR", value=403354)
+        years_birth = st.number_input("YEARS_BIRTH (Age)", value=62.239562)
+        years_employed = st.number_input("YEARS_EMPLOYED", value=5.10883)
+        years_registration = st.number_input("YEARS_REGISTRATION", value=24.454483)
+        years_id_publish = st.number_input("YEARS_ID_PUBLISH", value=12.755647)
+        cnt_fam_members = st.number_input("CNT_FAM_MEMBERS", value=2.0)
         occupation_type = st.text_input("OCCUPATION_TYPE", value="Laborers")
-        organization_type = st.text_input("ORGANIZATION_TYPE", value="Other")
-        region_pop_relative = st.number_input("REGION_POPULATION_RELATIVE", value=0.016166, format="%.6f")
+        organization_type = st.text_input("ORGANIZATION_TYPE", value="Business Entity Type 3")
+        region_pop_relative = st.number_input("REGION_POPULATION_RELATIVE", value=0.035792, format="%.6f")
 
     # -------------------------------
     # Column 2 – Financial & External
     # -------------------------------
     with col2:
         st.subheader("Financial & External")
-        amt_income_total = st.number_input("AMT_INCOME_TOTAL", value=60279.814153)
-        amt_credit = st.number_input("AMT_CREDIT", value=209720.185847)
-        amt_annuity = st.number_input("AMT_ANNUITY", value=11759.650076)
-        amt_goods_price = st.number_input("AMT_GOODS_PRICE", value=209720.185847)
-        ext_source_2 = st.number_input("EXT_SOURCE_2", value=0.355346, format="%.6f")
-        ext_source_3 = st.number_input("EXT_SOURCE_3", value=0.472510, format="%.6f")
-        own_car_age = st.number_input("OWN_CAR_AGE", value=27.843217)
-        years_last_phone_change = st.number_input("YEARS_LAST_PHONE_CHANGE", value=1.799217)
-        amt_req_credit_bureau_year = st.number_input("AMT_REQ_CREDIT_BUREAU_YEAR", value=1.320897)
+        amt_income_total = st.number_input("AMT_INCOME_TOTAL", value=211500.0)
+        amt_credit = st.number_input("AMT_CREDIT", value=883863.0)
+        amt_annuity = st.number_input("AMT_ANNUITY", value=25330.5)
+        amt_goods_price = st.number_input("AMT_GOODS_PRICE", value=737784.0)
+        ext_source_2 = st.number_input("EXT_SOURCE_2", value=0.768663, format="%.6f")
+        ext_source_3 = st.number_input("EXT_SOURCE_3", value=0.488455, format="%.6f")
+        own_car_age = st.number_input("OWN_CAR_AGE", value=4.0)
+        years_last_phone_change = st.number_input("YEARS_LAST_PHONE_CHANGE", value=0.0)
+        amt_req_credit_bureau_year = st.number_input("AMT_REQ_CREDIT_BUREAU_YEAR", value=2.0)
 
     # -----------------------------
     # Column 3 – Process & Social
     # -----------------------------
     with col3:
         st.subheader("Process & Social")
-        weekday_appr = st.text_input("WEEKDAY_APPR_PROCESS_START", value="THURSDAY")
-        hour_appr = st.number_input("HOUR_APPR_PROCESS_START", value=11)
-        obs_30 = st.number_input("OBS_30_CNT_SOCIAL_CIRCLE", value=3.395514)
-        obs_60 = st.number_input("OBS_60_CNT_SOCIAL_CIRCLE", value=3.395514)
+        weekday_appr = st.text_input("WEEKDAY_APPR_PROCESS_START", value="SATURDAY")
+        hour_appr = st.number_input("HOUR_APPR_PROCESS_START", value=14)
+        obs_30 = st.number_input("OBS_30_CNT_SOCIAL_CIRCLE", value=4.0)
+        obs_60 = st.number_input("OBS_60_CNT_SOCIAL_CIRCLE", value=4.0)
 
     # ----------------------------------------------
     # Previous Application History (Aggregated)
@@ -73,35 +73,35 @@ with st.form("prediction_form"):
     # Exp Column 1
     # -------------------
     with exp_col1:
-        prev_amt_annuity_mean = st.number_input("PREV_AMT_ANNUITY_MEAN", value=12966.179766)
-        prev_amt_annuity_median = st.number_input("PREV_AMT_ANNUITY_MEDIAN", value=12942.666027)
-        prev_amt_application_mean = st.number_input("PREV_AMT_APPLICATION_MEAN", value=142340.746101)
-        prev_amt_application_median = st.number_input("PREV_AMT_APPLICATION_MEDIAN", value=146824.481512)
-        prev_amt_credit_mean = st.number_input("PREV_AMT_CREDIT_MEAN", value=158812.206066)
-        prev_amt_credit_median = st.number_input("PREV_AMT_CREDIT_MEDIAN", value=164665.851405)
-        prev_amt_goods_price_mean = st.number_input("PREV_AMT_GOODS_PRICE_MEAN", value=142340.746101)
-        prev_amt_goods_price_median = st.number_input("PREV_AMT_GOODS_PRICE_MEDIAN", value=146824.481512)
-        prev_cnt_payment_max = st.number_input("PREV_CNT_PAYMENT_MAX", value=27.850766)
-        prev_sellerplace_area_mean = st.number_input("PREV_SELLERPLACE_AREA_MEAN", value=328.325012)
-        prev_sk_id_prev_count = st.number_input("PREV_SK_ID_PREV_COUNT", value=2.320897)
-        prev_sk_id_curr_first = st.number_input("PREV_SK_ID_CURR_FIRST", value=370868.586736)
+        prev_amt_annuity_mean = st.number_input("PREV_AMT_ANNUITY_MEAN", value=21739.23)
+        prev_amt_annuity_median = st.number_input("PREV_AMT_ANNUITY_MEDIAN", value=21739.23)
+        prev_amt_application_mean = st.number_input("PREV_AMT_APPLICATION_MEAN", value=0.0)
+        prev_amt_application_median = st.number_input("PREV_AMT_APPLICATION_MEDIAN", value=0.0)
+        prev_amt_credit_mean = st.number_input("PREV_AMT_CREDIT_MEAN", value=0.0)
+        prev_amt_credit_median = st.number_input("PREV_AMT_CREDIT_MEDIAN", value=0.0)
+        prev_amt_goods_price_mean = st.number_input("PREV_AMT_GOODS_PRICE_MEAN", value=0.0)
+        prev_amt_goods_price_median = st.number_input("PREV_AMT_GOODS_PRICE_MEDIAN", value=0.0)
+        prev_cnt_payment_max = st.number_input("PREV_CNT_PAYMENT_MAX", value=24.0)
+        prev_sellerplace_area_mean = st.number_input("PREV_SELLERPLACE_AREA_MEAN", value=-1.0)
+        prev_sk_id_prev_count = st.number_input("PREV_SK_ID_PREV_COUNT", value=1.0)
+        prev_sk_id_curr_first = st.number_input("PREV_SK_ID_CURR_FIRST", value=403354.0)
 
     # -------------------
     # Exp Column 2
     # -------------------
     with exp_col2:
-        prev_years_decision_mean = st.number_input("PREV_YEARS_DECISION_MEAN", value=2.364931)
-        prev_years_first_due_mean = st.number_input("PREV_YEARS_FIRST_DUE_MEAN", value=2.542393)
-        prev_years_last_due_mean = st.number_input("PREV_YEARS_LAST_DUE_MEAN", value=2.448745)
-        prev_years_last_due_1st_version_mean = st.number_input("PREV_YEARS_LAST_DUE_1ST_VERSION_MEAN", value=1.835616)
+        prev_years_decision_mean = st.number_input("PREV_YEARS_DECISION_MEAN", value=0.50924)
+        prev_years_first_due_mean = st.number_input("PREV_YEARS_FIRST_DUE_MEAN", value=2.220397)
+        prev_years_last_due_mean = st.number_input("PREV_YEARS_LAST_DUE_MEAN", value=2.672142)
+        prev_years_last_due_1st_version_mean = st.number_input("PREV_YEARS_LAST_DUE_1ST_VERSION_MEAN", value=1.626283)
         prev_years_first_drawing_mean = st.number_input("PREV_YEARS_FIRST_DRAWING_MEAN", value=999.980835)
-        prev_years_termination_mean = st.number_input("PREV_YEARS_TERMINATION_MEAN", value=2.450999)
-        prev_hour_appr_mean = st.number_input("PREV_HOUR_APPR_PROCESS_START_MEAN", value=14.679103)
+        prev_years_termination_mean = st.number_input("PREV_YEARS_TERMINATION_MEAN", value=2.688569)
+        prev_hour_appr_mean = st.number_input("PREV_HOUR_APPR_PROCESS_START_MEAN", value=13.0)
         prev_nflag_insured_max = st.number_input("PREV_NFLAG_INSURED_ON_APPROVAL_MAX", value=0.0)
 
-        prev_prod_comb = st.text_input("PREV_PRODUCT_COMBINATION_<LAMBDA>", value="POS industry with interest")
+        prev_prod_comb = st.text_input("PREV_PRODUCT_COMBINATION_<LAMBDA>", value="Cash")
         prev_goods_cat = st.text_input("PREV_NAME_GOODS_CATEGORY_<LAMBDA>", value="XNA")
-        prev_weekday_lambda = st.text_input("PREV_WEEKDAY_APPR_PROCESS_START_<LAMBDA>", value="MONDAY")
+        prev_weekday_lambda = st.text_input("PREV_WEEKDAY_APPR_PROCESS_START_<LAMBDA>", value="TUESDAY")
 
     submit_button = st.form_submit_button("Predict Status")
 
